fb182f1063cb8ede4e4acef87beb0e42a9d5e5ff
Zeke Gabrielse <ezekg@yahoo.com>
2024-08-09 16:48:17 -0500

    fix select star regex

--- lib/sql_matchers/normalizers/sql.rb
+++ lib/sql_matchers/normalizers/sql.rb
@@ -5,8 +5,8 @@ require 'anbt-sql-formatter/formatter'
 module SqlMatchers
   module Normalizers
     class Sql
+      SELECT_STAR_RE  = /[`"]?(\w+)[`"]?\s{,1}\.\s{,1}\*/
       QUOTED_IDENT_RE = /[`"](\w+)[`"]/
-      SELECT_STAR_RE  = /[`"]?(\w+)[`"]?\s{,1}\*/
 
       def normalize(sql) = +sql.gsub(SELECT_STAR_RE, '\1.*')
                                .gsub(QUOTED_IDENT_RE, '\1')
